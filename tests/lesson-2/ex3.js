c033519041318c56fbe66cdf68008f0f77d53559
Thong Vu <thongvuminh@gmail.com>
2025-02-25 02:25:04 +0700

    fix: minor typo of ex3

--- tests/lesson-2/ex3.js
+++ tests/lesson-2/ex3.js
@@ -4,16 +4,15 @@
 // ii. Nếu hằng số nhỏ hơn 0: “Giá trị bạn nhập là số âm” 
 // iii. Nếu hằng số = 0: “Giá trị bạn nhập là số 0” 
 
-const myNumber = 10;
+const myNumber = -5;
 
 if( myNumber > 0){
     console.log("Giá trị bạn nhập là số dương")
 } 
-if ( myNumber < 0 ){
+if (myNumber < 0){
     console.log("Giá trị bạn nhập là số âm")
 } 
-
-if (myName === 0){
+if (myNumber === 0){
     console.log("Giá trị bạn nhập là số 0")
 }
 
